84c62bf77acdf409b68ac4cc226293b89dbccce4
philfab <mpemploipo@gmail.com>
2024-04-05 16:46:17 +0200

    exo PDO gaulois details Persos

--- detailGaulois.php
+++ detailGaulois.php
@@ -1,2 +1,41 @@
 <?php
+session_start();
 
+if (isset($_POST['id_personnage'])) {
+    $idPersonnage = $_POST['id_personnage'];
+
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=gaulois;charset=utf8', 'root', '', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+
+        $req = $pdo->prepare("SELECT personnage.nom_personnage, lieu.nom_lieu, specialite.nom_specialite 
+                               FROM personnage
+                               JOIN lieu ON personnage.id_lieu = lieu.id_lieu
+                               JOIN specialite ON personnage.id_specialite = specialite.id_specialite
+                               WHERE personnage.id_personnage = ?");/* ? = placeholder */
+
+        $req->execute([$idPersonnage]);/* ? = [$idPersonnage] */
+
+        $detailsPerso = $req->fetch();
+
+        $req = $pdo->prepare("SELECT bataille.nom_bataille, bataille.date_bataille 
+                              FROM bataille
+                              JOIN prendre_casque ON bataille.id_bataille = prendre_casque.id_bataille
+                              WHERE prendre_casque.id_personnage = ?
+                              ORDER BY bataille.date_bataille DESC");
+
+        $req->execute([$idPersonnage]);
+        $batailles = $req->fetchAll();
+
+
+        $_SESSION['detailsPerso'] = $detailsPerso;
+        $_SESSION['batailles'] = $batailles;
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    // redirection vers listGaulois.php
+    header('Location: listGaulois.php');
+    exit();
+}
--- listGaulois.php
+++ listGaulois.php
@@ -1,17 +1,6 @@
-<link rel="stylesheet" type="text/css" href="style.css">
 <?php
-
-include 'detailGaulois.php';
-
-function rowClicked($nom_personnage) {
-    echo "click sur : " . $nom_personnage;
-}
-
-echo "<script>
-    function rowClicked(nom_personnage) {
-        alert('click sur : ' + nom_personnage);
-    }
-</script>";
+session_start();
+//---------------------------------------------------------------------------------------
 
 try {
     $pdo = new PDO(
@@ -25,30 +14,75 @@ try {
 }
 
 //---------------------------------------------------------------------------------------
-    
-$sqlQ1 = 
-    "SELECT nom_personnage, specialite.nom_specialite, lieu.nom_lieu FROM personnage
+
+$sql =
+    "SELECT id_personnage, nom_personnage, specialite.nom_specialite, lieu.nom_lieu FROM personnage
     INNER JOIN lieu ON personnage.id_lieu = lieu.id_lieu
     INNER JOIN specialite ON personnage.id_specialite = specialite.id_specialite";
 
-$perso = $pdo->prepare($sqlQ1);
+$perso = $pdo->prepare($sql);
 $perso->execute();
 $persos = $perso->fetchAll();
+?>
 
-echo "<table>
+<!-------------------------------------------------------------------------------------->
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<div class="container">
+    <table>
         <tr>
             <th>Nom</th>
             <th>Lieu</th>
             <th>Spécialité</th>
-        </tr>";
+        </tr>
+        <?php foreach ($persos as $index => $perso) : ?>
+            <tr onclick="submitForm(<?= $index ?>)"> <!-- click sur row -->
+                <td><?= $perso['nom_personnage']; ?></td><!-- '=' :  remplace php echo, plus concis et clair -->
+                <td><?= $perso['nom_lieu']; ?></td>
+                <td><?= $perso['nom_specialite']; ?></td>
+                <td class="td-form">
+                    <form id="form_<?= $index ?>" action="detailGaulois.php" method="post">
+                        <input type="hidden" name="id_personnage" value="<?= $perso['id_personnage']; ?>">
+                        <button type="submit">Voir détails</button>
+                    </form>
+                </td>
+            </tr>
+            <script>
+                function submitForm(index) {
+                    document.getElementById('form_' + index).submit();
+                };
+            </script>
+        <?php endforeach; ?><!-- endforeach pour meilleure lisibilité php -->
+    </table>
 
-foreach ($persos as $perso) {
-    echo "<tr onclick='rowClicked(\"" . $perso['nom_personnage'] . "\")'>
-            <td>" . $perso['nom_personnage'] . "</td>
-            <td>" . $perso['nom_lieu'] . "</td>
-            <td>" . $perso['nom_specialite'] . "</td>
-          </tr>";
-}
-echo "</table><br>";
+    <!-- affiche les détails du perso -->
+    <?php if (isset($_SESSION['detailsPerso'])) : ?>
+        <table>
+            <tr>
+                <th>Nom</th>
+                <th>Village</th>
+                <th>Spécialité</th>
+            </tr>
+            <tr>
+                <td><?= $_SESSION['detailsPerso']['nom_personnage'] ?></td>
+                <td><?= $_SESSION['detailsPerso']['nom_lieu'] ?></td>
+                <td><?= $_SESSION['detailsPerso']['nom_specialite'] ?></td>
+            </tr>
+        </table>
+    <?php endif; ?>
 
-?>
+    <div class="container-bataille">
+        <?php if (isset($_SESSION['batailles'])) : ?>
+            <h2>Batailles</h2>
+            <ul>
+                <?php foreach ($_SESSION['batailles'] as $bataille) : ?>
+                    <li><?= $bataille['date_bataille'] ?> - <?= $bataille['nom_bataille'] ?></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php
+            unset($_SESSION['detailsPerso'], $_SESSION['batailles']);
+        endif;
+        ?>
+    </div>
+</div>
